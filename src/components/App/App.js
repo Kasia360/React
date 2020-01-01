c3a9c9a1fbfc7ae0d21bbe5dd296651b6a57d081
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2020-01-02 00:55:27 +0100

    Components List/Hero

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
+import List from '../List/List';
+import Hero from '../Hero/Hero';
 
 class App extends React.Component {
   render() {
@@ -7,6 +9,8 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
+        <Hero />
+        <List />
       </main>
     )
   }
