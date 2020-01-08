949b0917cbeb87ddf95ebda8426b291da7e1864f
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2020-01-08 16:27:05 +0100

    add components

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -25,20 +25,20 @@ class Column extends React.Component {
           {
             key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
             title,
-          }
-        ]
+          },
+        ],
       }
     ));
   }
   render() {
     return (
       <section className={styles.component}>
-      <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon} /></span>{this.props.title}</h3>
-      {this.state.cards.map(({ key, ...cardProps }) => (
-        <Card key={key} {...cardProps}>
-        </Card>
-      ))}
-      <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon} /></span>{this.props.title}</h3>
+        {this.state.cards.map(({ key, ...cardProps }) => (
+          <Card key={key} {...cardProps}>
+          </Card>
+        ))}
+        <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
       </section>
     );
   }
