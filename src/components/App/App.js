8551cefa1508f83ef35d42405e87f9cac4a7d93d
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2020-01-15 04:21:34 +0100

    add section and switch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import FAQ from '../FAQ/FAQ';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import { AnimatedSwitch } from 'react-router-transition';
 
 const App = () => (
   <BrowserRouter>
-  <MainLayout>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/info' component={Info} />
-    </Switch>
+    <MainLayout>
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper"
+    >
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+        <Route exact path='/FAQ' component={FAQ} />
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
