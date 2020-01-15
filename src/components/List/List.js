9b6b07838af7a9ee2c693b04101e4c6948796d23
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2020-01-15 04:01:27 +0100

    add react router and nav

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -12,20 +12,20 @@ import Container from '../Container/Container';
 const List = ({ title, image, description, columns, addColumn }) => {
   return (
     <section className={styles.component}>
-    <Container>
-      <Hero titleText={title} imagePath={image} />
-      <div className={styles.description}>
-        {ReactHtmlParser(description)}
-      </div>
+      <Container>
+        <Hero titleText={title} imagePath={image} />
+        <div className={styles.description}>
+          {ReactHtmlParser(description)}
+        </div>
 
-      <div className={styles.columns}>
-        {columns.map(columnData => (
-          <Column key={columnData.id} {...columnData} />
-        ))}
-      </div>
-      <div className={styles.creator}>
-        <Creator text={settings.columnCreatorText} action={addColumn} />
-      </div>
+        <div className={styles.columns}>
+          {columns.map(columnData => (
+            <Column key={columnData.id} {...columnData} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={addColumn} />
+        </div>
       </Container>
     </section>
   );
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './Search.scss';
 import Button from '../Button/Button';
+import Icon from '../Icon/Icon';
+import Container from '../Container/Container';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
-import Icon from '../Icon/Icon';
+
 
 class Search extends React.Component {
   static propTypes = {
@@ -45,6 +47,7 @@ class Search extends React.Component {
     const {icon} = settings.search;
     return (
       <div className={styles.component}>
+      <Container>
         <input
           type='text'
           placeholder={text}
@@ -57,6 +60,7 @@ class Search extends React.Component {
         <div>
           { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
         </div>
+        </ Container>
       </div>
     );
   }
